c9e790643b8ab3d75f00a36779e5d19b9e1f490e
Ricardo Dias <rycardo.dias@hotmail.com>
2023-05-07 23:48:50 +0100

    .

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,8 +1,7 @@
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet'
-
 import { icon } from 'leaflet'
-import MapsCoordinates from 'interfaces/MapsCoordinates';
+import { MapNodeType, MapArcType } from 'interfaces/MapsCoordinates';
 
 const ICON = icon({
     iconUrl: "/flags.png",
@@ -10,12 +9,11 @@ const ICON = icon({
 })
 
 type MapProps = {
-    markers: Array<MapsCoordinates>
+    markers: Array<MapNodeType>
+    arcs: Array<MapArcType>
 }
 
-
-
-const Map = ({ markers }: MapProps) => {
+const Map = ({ markers, arcs }: MapProps) => {
 
     return (
         <MapContainer center={[41.6946, -8.83016]} zoom={8} scrollWheelZoom={false} style={{ height: "89vh" }}>
@@ -23,21 +21,40 @@ const Map = ({ markers }: MapProps) => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            {markers.length && markers.map(item => {
+            {markers.length > 0 && markers.map(item => {
                 return (
-                    <Marker position={[item.lat, item.lng]} icon={ICON}>
+                    <Marker key={item.ID} position={[item.lat, item.lng]} icon={ICON}>
                         <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
+                            {item.ID}
                         </Popup>
                     </Marker>
                 )
-            })}
-            <Polyline color='pink' 
-                positions={markers.map(item => {
-                    return [item.lat, item.lng]
-                })} />
-        </MapContainer>
+            })
+            }
+            {arcs.length > 0 && arcs.map(arc => {
+                const initialNode = markers.find(marker => marker.ID === arc.graphsInfo.initialNode) || { ID: '', lat: 0, lng: 0 }
+                const finalNode = markers.find(marker => marker.ID === arc.graphsInfo.finalNode) || { ID: '', lat: 0, lng: 0 }
+
+                return (
+                    <Polyline key={arc.ID} color='black'
+                        positions={
+                            [
+                                { lat: initialNode.lat, lng: initialNode.lng },
+                                { lat: finalNode.lat, lng: finalNode.lng }
+                            ]}
+                        eventHandlers={{
+                            click: (e) => {
+                                // const closest = getSegment(e.latlng, e.sourceTarget);
+                                // setResults([...results, closest]);
+                                console.log(arc.ID)
+                            }
+                        }}
+                    >
+                    </Polyline >
+                )
+            })
+            }
+        </MapContainer >
     )
 }
-
 export default Map
--- interfaces/MapsCoordinates.ts
+++ interfaces/MapsCoordinates.ts
@@ -1,4 +1,17 @@
-export default interface MapsCoordinates {
+interface MapsCoordinates {
     lat: number;
     lng: number;
+}
+
+export interface MapNodeType extends MapsCoordinates {
+    ID: string;
+}
+
+export interface MapArcType {
+    ID: string;
+    graphsInfo: {
+        initialNode: string;
+        finalNode: string;
+    }
+
 }
